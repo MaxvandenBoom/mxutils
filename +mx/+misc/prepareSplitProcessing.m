178842c5bd083029994a9f123f6d2eee4f715874
Max <m.a.vandenboom84@gmail.com>
2019-12-03 14:34:07 -0600

    Small aestheric updates of descriptions

--- +mx/+misc/prepareSplitProcessing.m
+++ +mx/+misc/prepareSplitProcessing.m
@@ -1,3 +1,4 @@
+%	
 %   Prepare for split processing, splitting up the input for speed
 %   optimization (multi-threading) or memory maximization
 %
@@ -24,7 +25,7 @@
 %
 %   Copyright (c) 2019, Max van den Boom
 
-% 	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+%   This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
 %   as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 %   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
 %   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
